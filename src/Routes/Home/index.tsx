7b89b83b8ea56adbce1977bdd8afff9fa9573b14
soorajUM <soorajushamurali13@gmail.com>
2024-02-19 21:39:23 -0800

    T24-1039/PRODUCT DETAILS DONE

--- src/Routes/Home/index.tsx
+++ src/Routes/Home/index.tsx
@@ -1,18 +1,5 @@
-import Form from "../../Components/InfraStructure/Form";
-import ProductCard from "../ProductCard";
-
-import { validateRegister } from "../../Validation";
-import MoreDetails from "../MoreDetails";
-import RelatedProducts from "../RelatedProducts";
-
 const Home = () => {
-  return (
-    <div className="flex flex-col justify-center items-center">
-      <ProductCard />
-      <MoreDetails />
-      <RelatedProducts />
-    </div>
-  );
+  return <></>;
 };
 
 export default Home;
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,14 +1,14 @@
-import {Route,Routes} from "react-router-dom"
-import Home from "./Home"
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import ProductDetails from "./ProductDetails";
 
 const Router = () => {
   return (
     <Routes>
-        <Route path="" element={<Home/>} />
-        <Route path="/hello" element={<div>Hello</div>} />
+      <Route path="" element={<Home />} />
+      <Route path="/ProductDetails" element={<ProductDetails />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
