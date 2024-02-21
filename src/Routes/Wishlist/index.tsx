2fb753e59bae304621614e89dfcb21e70522e327
majmalmdt <adarshputhuparambil324@gmail.com>
2024-02-21 13:26:38 +0530

    removed unwanted code

--- src/Routes/Wishlist/index.tsx
+++ src/Routes/Wishlist/index.tsx
@@ -17,8 +17,8 @@ const Wishlist = () =>{
 <div>
     <div className="grid grid-cols-4 gap-2 space-y-5 place-items-center">
         {data?.map((product,index)=>(
-      <Product key={index} image={product.image} names={product.productName} price={product.price} first={bluecart}
-      second={bluedelete} third={bluepluse} id={product._id}/>  
+      <Product key={index} image={product.image} names={product.productName} price={product.price} 
+       id={product._id}/>  
       ))}     
     </div>
 </div>
