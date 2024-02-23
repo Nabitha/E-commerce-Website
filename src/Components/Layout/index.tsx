4fdf45837b294e241100beb891fcc79a4743e027
soorajUM <soorajushamurali13@gmail.com>
2024-02-23 00:21:28 -0800

    code refactor

--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -32,28 +32,43 @@ const Layout = ({ children }: PropsTypes) => {
   useEffect(() => {
     setPathName(location.pathname);
   }, [location.pathname]);
-    useEffect(()=>{
-        reload()
-    },
-    [data])
-    
-    const navigate = useNavigate();
-    return (
-        <>
-         <div className="sticky top-0 z-50 bg-violet-600 text-white font-semibold flex items-center justify-around  py-4 pr-10"> 
-         <div className="flex items-center gap-16">
-                <span><img src={logo}/></span>
-                <span className="flex gap-4 font-medium  ">
-                <span className="hover:text-pink-500">Home </span>
-                <span className={`${("/products"===pathName)?'text-pink-500':' hover:text-pink-500'}`}><Link to="/products">Products</Link></span>
-                </span>
-                </div>
-                <div className="relative w-full max-w-80 max-lg:max-w-36 ">
-                    <input type="text" placeholder="Search. . ." className="border-2 rounded border-blueGray-200 text-black px-2 pr-10 w-full"/> 
-                <button className="p-1 rounded right-0 absolute"><img className="w-6" src={search} />
-                </button>
-                </div>
-            {/* <div className="flex  gap-4">
+  // useEffect(()=>{
+  //     reload()
+  // },
+  // [data])
+
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="sticky top-0 z-50 bg-violet-600 text-white font-semibold flex items-center justify-around  py-4 pr-10">
+        <div className="flex items-center gap-16">
+          <span>
+            <img src={logo} />
+          </span>
+          <span className="flex gap-4 font-medium  ">
+            <span className="hover:text-pink-500">Home </span>
+            <span
+              className={`${
+                "/products" === pathName
+                  ? "text-pink-500"
+                  : " hover:text-pink-500"
+              }`}
+            >
+              <Link to="/products">Products</Link>
+            </span>
+          </span>
+        </div>
+        <div className="relative w-full max-w-80 max-lg:max-w-36 ">
+          <input
+            type="text"
+            placeholder="Search. . ."
+            className="border-2 rounded border-blueGray-200 text-black px-2 pr-10 w-full"
+          />
+          <button className="p-1 rounded right-0 absolute">
+            <img className="w-6" src={search} />
+          </button>
+        </div>
+        {/* <div className="flex  gap-4">
                 <Link to="/login">
                 <span className={`${("/login"===pathName)?'text-pink-500 flex gap-1':' flex gap-1 hover:text-pink-500'}`}>Login<img className="w-5" src={login}/></span>
                  </Link>
@@ -70,44 +85,54 @@ const Layout = ({ children }: PropsTypes) => {
                     </Link>
                     </span>
             </div> */}
-                   <div className="flex  items-center gap-8">
-         
-            
+        <div className="flex  items-center gap-8">
+          <span
+            className={`${
+              "/login" === pathName
+                ? "text-pink-500 flex  gap-1"
+                : " flex gap-1 hover:text-pink-500"
+            }`}
+          >
+            {checktoken() ? (
+              <span>{user?.name} </span>
+            ) : (
+              <Link to="/login"> Login </Link>
+            )}
+            <span>
+              <img src={login} />
+            </span>
+          </span>
+
+          {checktoken() ? (
+            <Link to="/wishlist">
               <span
                 className={`${
-                  "/login" === pathName
-                    ? "text-pink-500 flex  gap-1"
+                  "/wishlist" === pathName
+                    ? "text-pink-500 flex gap-1"
                     : " flex gap-1 hover:text-pink-500"
                 }`}
-              >{checktoken() ? <span>{user?.name} </span>: <Link to="/login"> Login </Link>} 
-              <span><img src={login} /></span>
-                
-                 </span>
-            
-          
-                 {checktoken() ? <Link to="/wishlist">
-            <span
-              className={`${
-                "/wishlist" === pathName
-                  ? "text-pink-500 flex gap-1"
-                  : " flex gap-1 hover:text-pink-500"
-              }`}
-            >
-              Wishlist
-              <img className="w-5" src={heart} />
-            </span>
-          </Link>:""}
+              >
+                Wishlist
+                <img className="w-5" src={heart} />
+              </span>
+            </Link>
+          ) : (
+            ""
+          )}
           <span className="relative ">
-          {checktoken() ? <Link to="/cart">
-              <img
-                className="w-6"
-                src={"/cart" === pathName ? pinkcart : cart}
-              />
-            <div className="absolute flex items-center justify-center -right-3  -top-3 w-5 h-5 rounded-full bg-white   text-md text-pink-500 ">
-              <div> {data?.length ||0}</div>
-            </div>
-            </Link>:""}
-
+            {checktoken() ? (
+              <Link to="/cart">
+                <img
+                  className="w-6"
+                  src={"/cart" === pathName ? pinkcart : cart}
+                />
+                <div className="absolute flex items-center justify-center -right-3  -top-3 w-5 h-5 rounded-full bg-white   text-md text-pink-500 ">
+                  <div> {data?.length || 0}</div>
+                </div>
+              </Link>
+            ) : (
+              ""
+            )}
           </span>
           <span>
             {checktoken() ? (
@@ -122,13 +147,10 @@ const Layout = ({ children }: PropsTypes) => {
               ""
             )}
           </span>
-      </div>
-         </div>     
-            <div className="flex gap-96 py-2 justify-center">         
-            </div>
-        <div className="p-8">
-        {children}
         </div>
+      </div>
+      <div className="flex gap-96 py-2 justify-center"></div>
+      <div className="p-8">{children}</div>
       <div className="pt-5">
         <div className="flex justify-center justify-around gap-12 bg-indigo-50 py-8 text-indigo-400">
           <div className="flex flex-col gap-2">
